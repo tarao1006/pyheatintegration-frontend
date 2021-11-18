8eafb38aaab83a640e626906de5f04f1ef976658
tarao1006 <taiga4562@gmail.com>
2021-11-18 18:32:30 +0900

    Update privacy policy page

--- src/PrivacyPolicy.tsx
+++ src/PrivacyPolicy.tsx
@@ -1,7 +1,9 @@
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import Footer from './Footer'
 import Header from './Header'
 import StyledLink, { OuterLink } from './Link'
 
@@ -17,7 +19,7 @@ const PrivacyPolicy = () => {
       }}
     >
       <Header />
-      <Container maxWidth="lg" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', alignItems: 'flex-start', minHeight: '72px' }}>
+      <Container maxWidth="sm" sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'left', alignItems: 'flex-start', minHeight: '72px' }}>
         <Box sx={{ mt: 1 }} />
 
         <StyledLink to="/">
@@ -26,14 +28,43 @@ const PrivacyPolicy = () => {
           </Button>
         </StyledLink>
 
-        <ul>
-          <li>Third party vendors, including Google, use cookies to serve ads based on a user's prior visits to your website or other websites.</li>
+        <Box sx={{ mt: 4, mb: 2 }}>
+          <Typography variant="h4">
+            Privacy Policy
+          </Typography>
+        </Box>
 
-          <li>Google's use of advertising cookies enables it and its partners to serve ads to your users based on their visit to your sites and/or other sites on the Internet.</li>
+        <Box sx={{ mb: 1 }}>
+          <Typography variant="body2">
+            November 18, 2021
+          </Typography>
+        </Box>
 
-          <li>Users may opt out of personalized advertising by visiting <OuterLink href="https://www.google.com/settings/ads" target="_blank">Ads Settings</OuterLink>. (Alternatively, you can direct users to opt out of a third-party vendor's use of cookies for personalized advertising by visiting <OuterLink href="http://www.aboutads.info/choices/" target="_blank">www.aboutads.info</OuterLink>.)</li>
-        </ul>
+        <Box sx={{ my: 2 }}>
+          <Typography variant="h5">
+            Google AdSense
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{ mb: 1 }}
+        >
+          Third party vendors, including Google, use cookies to serve ads based on a user's prior visits to your website or other websites.
+        </Box>
+
+        <Box
+          sx={{ mb: 1 }}
+        >
+          Google's use of advertising cookies enables it and its partners to serve ads to your users based on their visit to your sites and/or other sites on the Internet.
+        </Box>
+
+        <Box
+          sx={{ mb: 1 }}
+        >
+        Users may opt out of personalized advertising by visiting <OuterLink href="https://www.google.com/settings/ads" target="_blank">Ads Settings</OuterLink>. (Alternatively, you can direct users to opt out of a third-party vendor's use of cookies for personalized advertising by visiting <OuterLink href="http://www.aboutads.info/choices/" target="_blank">www.aboutads.info</OuterLink>.)
+        </Box>
       </Container>
+      <Footer />
     </Box>
   )
 }
